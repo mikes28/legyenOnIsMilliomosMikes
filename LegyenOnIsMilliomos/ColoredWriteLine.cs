bd097e5962d58285562e8c742203644e3cafbea5
mikes28 <mikeskrisztian06@gmail.com>
2025-05-16 15:02:25 +0200

    created ColoredWrite class, that prints colored text

---
+++ LegyenOnIsMilliomos/ColoredWriteLine.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LegyenOnIsMilliomos
+{
+    internal class ColoredWrite
+    {
+
+        public static void Line(string text, ConsoleColor font = ConsoleColor.White, ConsoleColor background = ConsoleColor.Black)
+        {
+
+
+            Console.ForegroundColor = font;
+            Console.BackgroundColor = background;
+            Console.WriteLine(text);
+            Console.ResetColor();
+
+
+        }
+    }
+}
