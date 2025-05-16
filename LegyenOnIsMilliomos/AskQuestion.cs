ae37dbde8c7aa97d8623dcbb18a1c7b97c8498dc
mikes28 <mikeskrisztian06@gmail.com>
2025-05-16 15:22:42 +0200

    ran git add * to upload forgoten things

---
+++ LegyenOnIsMilliomos/AskQuestion.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LegyenOnIsMilliomos
+{
+    internal class AskQuestion(Question question, bool _isItOrder = false)
+    {
+        //todo ask question, decide it the ans is good. return bool. should make a func for colored print, and try to amke a good looking font based gui
+    }
+}
---
+++ LegyenOnIsMilliomos/Game.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LegyenOnIsMilliomos
+{
+    internal class Game
+    {
+        //todo ask first question using Ask Question from the sorQuestions list, make it ask a type for the question
+        //for 10 time
+        //get random number from tha ques list, and run ask question, put the num on a list of used before. It should return true or false based on the correction of answer. 
+        //every time check wether the ques was asked before and if so, generate a new.
+    }
+}
