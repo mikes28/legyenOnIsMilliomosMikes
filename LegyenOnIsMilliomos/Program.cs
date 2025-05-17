34b3dd97211106bb0d58a9ef7bd55ab4fcf531b5
mikes28 <mikeskrisztian06@gmail.com>
2025-05-17 21:22:01 +0200

    edited it so teh app uses Game.cs instead of running in Program.cs

--- LegyenOnIsMilliomos/Program.cs
+++ LegyenOnIsMilliomos/Program.cs
@@ -4,22 +4,29 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
         var fileAction = new FileAction();
         List<Question> normQuestions = await fileAction.ReadFileAsync("kerdes");
         List<Question> sorQuestions = await fileAction.ReadFileAsync("sorkerdes");
 
-        System.Console.WriteLine(normQuestions[0]);
-        System.Console.WriteLine(normQuestions[1]);
-
-        ColoredWrite.Line(sorQuestions[5].Ques, ConsoleColor.Green, ConsoleColor.Black);
-        ColoredWrite.Line("Hello", ConsoleColor.Yellow, ConsoleColor.Blue);
-        System.Console.WriteLine(sorQuestions[0]);
-        System.Console.WriteLine(sorQuestions[1]);
+        Game game = new(sorQuestions, normQuestions);
 
 
-        System.Console.WriteLine(normQuestions[0].IsItCorrect(['B'])?"mukodk":"nem");
-        System.Console.WriteLine(sorQuestions[1].IsItCorrect(['B','c','a','d'])?"mukodk":"nem");
+        game.StartGame();
+
+
+        /*         
+
+
+            System.Console.WriteLine(normQuestions[0]);
+            System.Console.WriteLine(normQuestions[1]);
+
+            ColoredWrite.Line(sorQuestions[5].Ques, ConsoleColor.Green, ConsoleColor.Black);
+            ColoredWrite.Line("Hello", ConsoleColor.Yellow, ConsoleColor.Blue);
+            System.Console.WriteLine(sorQuestions[0]);
+            System.Console.WriteLine(sorQuestions[1]);
+
+
+            System.Console.WriteLine(normQuestions[0].IsItCorrect(['B'])?"mukodk":"nem");
+            System.Console.WriteLine(sorQuestions[1].IsItCorrect(['B','c','a','d'])?"mukodk":"nem"); */
     }
 }
