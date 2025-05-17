b9bbd136ad9dc45342d8b2f89f9583832410fb02
mikes28 <mikeskrisztian06@gmail.com>
2025-05-17 21:15:13 +0200

    AskQuestion func sort of works, needs some touch ups, needs help inplementation, and writeline formating

--- LegyenOnIsMilliomos/AskQuestion.cs
+++ LegyenOnIsMilliomos/AskQuestion.cs
@@ -5,8 +5,45 @@ using System.Threading.Tasks;
 
 namespace LegyenOnIsMilliomos
 {
-    internal class AskQuestion(Question question, bool _isItOrder = false)
+    internal class AskQuestion
     {
+        internal static bool Ask(Question question, bool _isItOrder = false)
+        {
+
+            System.Console.WriteLine("szia a kerdesed:");
+
+            System.Console.WriteLine($"Kategoriaja {question.Category}");
+            System.Console.WriteLine(question.Ques);
+            System.Console.WriteLine($"A: {question.Ans[0]}");
+            System.Console.WriteLine($"B: {question.Ans[1]}");
+            System.Console.WriteLine($"C: {question.Ans[2]}");
+            System.Console.WriteLine($"D: {question.Ans[3]}");
+            if (_isItOrder)
+            {
+                System.Console.WriteLine("kerlek gepeld be a helyes sorrendet majd nyomj entert. A betuket szokozok nelkul ird egymas utan.");
+                if (question.IsItCorrect(Console.ReadLine().ToString().ToCharArray()))
+                {
+                    System.Console.WriteLine("jo valasz");
+                    return true;
+
+                }
+                System.Console.WriteLine("sajnos rossz valasz");
+                return false;
+            }
+            else
+            {
+                System.Console.WriteLine("Kerlek nyomd le a megoldas betujet, vagy a szokozt!");
+                if (question.IsItCorrect([Console.ReadKey().KeyChar]))
+                {
+                    System.Console.WriteLine("jo valasz");
+                    return true;
+
+                }
+                System.Console.WriteLine("sajnos rossz valasz");
+                return false;
+            }
+
+        }
         //todo ask question, decide it the ans is good. return bool. should make a func for colored print, and try to amke a good looking font based gui
     }
 }
