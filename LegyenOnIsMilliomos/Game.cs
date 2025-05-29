5725e3d4b0fd7d15256c749f4179d440e81d8630
mikes28 <mikeskrisztian06@gmail.com>
2025-05-29 18:23:33 +0200

    updated Game.cs so the full game runs, very feature implemented, sort of tested

--- LegyenOnIsMilliomos/Game.cs
+++ LegyenOnIsMilliomos/Game.cs
@@ -1,20 +1,19 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
-using System.Linq;
-using System.Net.Quic;
-using System.Threading.Tasks;
 
 namespace LegyenOnIsMilliomos
 {
-
     internal class Game
     {
-        internal int level = 0;//level 0 is the entry question, 1 is the level 1
+        internal int level = 0; // level 0 is entry question, 1 is level 1
 
 
-        internal int[] prizeAmounts = new int[]
+
+        private HelpHandler helpHandler = new HelpHandler();
+
+        internal int[] prizeAmounts =
         {
+            0,
             10000,
             20000,
             50000,
@@ -32,33 +31,24 @@ namespace LegyenOnIsMilliomos
             50000000
         };
 
-
         private static Random rnd = new();
 
         private List<Question> normQuestions;
         private List<Question> sorQuestions;
-        internal string CompletedPhase => level switch
-        {
-            0 => "startup",
-            15 => "completed",
-            > 15 => "undefined",
-            >= 10 => "second",
-            >= 5 => "first",
-            _ => "not achieved"
-        };
 
         public Game(List<Question> sorQuestions, List<Question> normQuestions)
         {
             this.sorQuestions = sorQuestions;
             this.normQuestions = normQuestions;
         }
+
         public void StartGame()
         {
             Console.Clear();
 
             ColoredWrite.Write("Üdvözöllek a ", ConsoleColor.Black, ConsoleColor.White);
             ColoredWrite.Write("Legyen Ön is Milliomos", ConsoleColor.Yellow, ConsoleColor.DarkBlue);
-            ColoredWrite.Write(" játékban!\n", ConsoleColor.Black, ConsoleColor.White);
+            ColoredWrite.Write(" játékban!\n\n", ConsoleColor.Black, ConsoleColor.White);
 
             ColoredWrite.Write("Ebben a játékban 15 egyre nehezebb kérdést kell megválaszolnod.\n", ConsoleColor.White, ConsoleColor.Black);
 
@@ -71,60 +61,57 @@ namespace LegyenOnIsMilliomos
             ColoredWrite.Write("Ha hibázol, a garantált nyereményig visszazuhansz, így nem távozol üres kézzel.\n", ConsoleColor.White, ConsoleColor.Black);
 
             ColoredWrite.Write("Az első kérdés helyes megválaszolásával ", ConsoleColor.Green, ConsoleColor.Black);
-            ColoredWrite.Write("10 000 Ft-ot nyersz.", ConsoleColor.White, ConsoleColor.Black);
+            ColoredWrite.Write("10 000 Ft-ot nyersz.\n", ConsoleColor.White, ConsoleColor.Black);
 
             ColoredWrite.Write("Minden kérdés után a nyereményed körülbelül ", ConsoleColor.Green, ConsoleColor.Black);
-            ColoredWrite.Write("megduplázódik.\n", ConsoleColor.White, ConsoleColor.Black);
+            ColoredWrite.Write("megduplázódik.\n\n", ConsoleColor.White, ConsoleColor.Black);
 
-            ColoredWrite.Write("A játékot megelőzi egy gyorsasági „sorkérdés”, ahol a helyes válaszok helyes sorrendjét kell megtalálnod.", ConsoleColor.Magenta, ConsoleColor.Black);
-            ColoredWrite.Write("A leggyorsabb helyes válaszadó kerül be a játékba.\n", ConsoleColor.Magenta, ConsoleColor.Black);
+            ColoredWrite.Write("A játékot megelőzi egy gyorsasági „sorkérdés”, ahol a helyes válaszok helyes sorrendjét kell megtalálnod.\n", ConsoleColor.Magenta, ConsoleColor.Black);
+            ColoredWrite.Write("A leggyorsabb helyes válaszadó kerül be a játékba.\n\n", ConsoleColor.Magenta, ConsoleColor.Black);
 
             ColoredWrite.Write("Készen állsz? Akkor kezdjük is el a játékot! ", ConsoleColor.Yellow, ConsoleColor.Black);
-            ColoredWrite.Write("(Készen állsz? nyomj meg tetszoleges billentyut)\n", ConsoleColor.DarkYellow, ConsoleColor.Black);
+            ColoredWrite.Write("(Készen állsz? nyomj meg tetszőleges billentyűt)\n", ConsoleColor.DarkYellow, ConsoleColor.Black);
 
             Console.ReadKey();
             StartQues();
-
         }
 
         private void StartQues()
         {
-            Question selQues = sorQuestions[rnd.Next(sorQuestions.Count)];
-            System.Console.Write("\n");
-            System.Console.Write("\n");
-            ColoredWrite.Write("\nKezdésként következik egy gyorsasági feladat:", ConsoleColor.Magenta, ConsoleColor.Black);
+            Console.Clear();
+            ColoredWrite.Write("\nKezdésként következik egy gyorsasági feladat:\n", ConsoleColor.White, ConsoleColor.Black);
             ColoredWrite.Write("Ez a híres-hírhedt ", ConsoleColor.Magenta, ConsoleColor.Black);
             ColoredWrite.Write("\"sorkérdés\"!\n", ConsoleColor.Yellow, ConsoleColor.Black);
 
             ColoredWrite.Write("A feladatod: ", ConsoleColor.White, ConsoleColor.Black);
-            ColoredWrite.Write("helyezd a megadott négy elemet a megfelelő sorrendbe.", ConsoleColor.Cyan, ConsoleColor.Black);
+            ColoredWrite.Write("helyezd a megadott négy elemet a megfelelő sorrendbe.\n", ConsoleColor.Cyan, ConsoleColor.Black);
 
-            ColoredWrite.Write("Lehet ez időrendi, nagyságrendi vagy logikai sorrend is - figyelj jól!", ConsoleColor.White, ConsoleColor.Black);
-            ColoredWrite.Write("Csak a leggyorsabb helyes válasz jut tovább a játékba!\n", ConsoleColor.Yellow, ConsoleColor.Black);
+            ColoredWrite.Write("Lehet ez időrendi, nagyságrendi vagy logikai sorrend is - figyelj jól!\n", ConsoleColor.White, ConsoleColor.Black);
+            ColoredWrite.Write("Csak a leggyorsabb helyes válasz jut tovább a játékba!\n\n", ConsoleColor.Yellow, ConsoleColor.Black);
 
+            bool isAnsCorrect;
+            int tries = 0;
 
-            bool is_AnsCorrect;
-            int i = 0;
             do
             {
-                is_AnsCorrect = AskQuestion.Ask(selQues, true);
-                System.Console.WriteLine("kerdes?");
-                i += 1;
-            } while (!is_AnsCorrect && i < 10);
+                int index = rnd.Next(sorQuestions.Count);
+                Question selQues = sorQuestions[index];
+                isAnsCorrect = AskQuestion.Ask(selQues, true);
+                tries++;
+                sorQuestions.RemoveAt(index);
+            } while (!isAnsCorrect && tries < 10);
 
-            if (is_AnsCorrect)
+            if (isAnsCorrect)
             {
-                System.Console.WriteLine("Gratulalok, jol valaszoltal!");
+                ColoredWrite.Write("\nGratulálok, jól válaszoltál!\n", ConsoleColor.Green, ConsoleColor.Black);
                 level = 1;
                 NormQues();
             }
             else
             {
-                System.Console.WriteLine("Sajnalom, de a mai nap nem sikerult bekerulnod a jatekba! :c");
+                ColoredWrite.Write("\nSajnálom, de a mai nap nem sikerült bekerülnöd a játékba! :(\n", ConsoleColor.Red, ConsoleColor.Black);
             }
-
         }
-
 
         private void NormQues()
         {
@@ -136,7 +123,7 @@ namespace LegyenOnIsMilliomos
             ColoredWrite.Write("kapsz, melyek közül csak egy helyes.\n", ConsoleColor.White, ConsoleColor.Black);
 
             ColoredWrite.Write("Az első kérdés ", ConsoleColor.White, ConsoleColor.Black);
-            ColoredWrite.Write($"{prizeAmounts[level - 1]} Ft-ot ér, ", ConsoleColor.Green, ConsoleColor.Black);
+            ColoredWrite.Write($"{prizeAmounts[level]} Ft-ot ér, ", ConsoleColor.Green, ConsoleColor.Black);
             ColoredWrite.Write("és minden további válasz után nő a nyereményed!\n", ConsoleColor.Green, ConsoleColor.Black);
 
             ColoredWrite.Write("Az 5. és a 10. kérdés után garantált összeget kapsz - ", ConsoleColor.Yellow, ConsoleColor.Black);
@@ -147,15 +134,50 @@ namespace LegyenOnIsMilliomos
             ColoredWrite.Write("A segítségek és a megoldás beírása között a ", ConsoleColor.White, ConsoleColor.Black);
             ColoredWrite.Write("SZÓKÖZ ", ConsoleColor.Yellow, ConsoleColor.Black);
             ColoredWrite.Write("gombbal tudsz váltani.\n", ConsoleColor.White, ConsoleColor.Black);
-            Question selQues = normQuestions[rnd.Next(normQuestions.Count)];
-            Console.WriteLine(AskQuestion.Ask(selQues) ? "Gratulálok!" : "Ez most nem sikerült.");
 
+            int index = 0;
 
+            while (level <= 15 && normQuestions.Count > 0)
+            {
+                index = rnd.Next(normQuestions.Count);
+                ColoredWrite.Write($"\nAz {level}. kérdésed {prizeAmounts[level]} Ft összegért:\n", ConsoleColor.White, ConsoleColor.Black);
+                ColoredWrite.Write($"Eddigi biztos nyeremény: {prizeAmounts[Math.Max(level - 1, 0)]}\n", ConsoleColor.White, ConsoleColor.Black);
+
+                Question selQues = normQuestions[index];
+                bool correct = AskQuestion.Ask(selQues);
+
+                if (correct)
+                {
+                    ColoredWrite.Write("Jól válaszoltál!\n", ConsoleColor.Green, ConsoleColor.Black);
+                    level++;
+                    normQuestions.RemoveAt(index);
+                }
+                else
+                {
+                    ColoredWrite.Write("Rossz válasz.\n", ConsoleColor.Red, ConsoleColor.Black);
+                    int guaranteed = 0;
+                    if (level >= 10) guaranteed = prizeAmounts[10];
+                    else if (level >= 5) guaranteed = prizeAmounts[5];
+
+                    Failed(level, guaranteed);
+                    return;
+                }
+            }
+
+            if (level > 15)
+            {
+                Won(prizeAmounts[15]);
+            }
         }
 
+        private void Failed(int level, int prize)
+        {
+            ColoredWrite.Write($"Sajnos vesztettél a(z) {level}. szinten. Nyereményed: {prize} Ft.\n", ConsoleColor.Red, ConsoleColor.Black);
+        }
+
+        private void Won(int prize)
+        {
+            ColoredWrite.Write($"Gratulálok, nyertél {prize} Ft-ot!\n", ConsoleColor.Green, ConsoleColor.Black);
+        }
     }
-    //todo ask first question using Ask Question from the sorQuestions list, make it ask a type for the question
-    //for 10 time
-    //get random number from tha ques list, and run ask question, put the num on a list of used before. It should return true or false based on the correction of answer. 
-    //every time check wether the ques was asked before and if so, generate a new.
 }
